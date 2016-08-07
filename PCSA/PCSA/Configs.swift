59399f641eecde8710c557049888829875909c1f
chamika <chamikaw@gmail.com>
2016-08-08 01:01:02 +0530

    Add Login functionality

---
+++ PCSA/PCSA/Configs.swift
@@ -0,0 +1,16 @@
+//
+//  Configs.swift
+//  PCSA
+//
+//  Created by Chamika Weerasinghe on 8/7/16.
+//  Copyright © 2016 Peacecorps. All rights reserved.
+//
+
+import Foundation
+
+class Configs{
+    static let KEY_LOGGED = "KEY_LOGGED"
+    static let KEY_NAME = "KEY_NAME"
+    static let KEY_LOCATION = "KEY_LOCATION"
+
+}
--- PCSA/PCSA/SpashViewController.swift
+++ PCSA/PCSA/SpashViewController.swift
@@ -9,22 +9,33 @@
 import UIKit
 
 class SplashViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        performSelector(#selector(SplashViewController.showMainController), withObject: nil, afterDelay: 0)
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let logged = defaults.boolForKey(Configs.KEY_LOGGED)
+        
+        if(logged){
+            performSelector(#selector(SplashViewController.showMainController), withObject: nil, afterDelay: 0)
+        }else{
+            performSelector(#selector(SplashViewController.showLoginController), withObject: nil, afterDelay: 0)
+        }
     }
     
     func showMainController(){
         performSegueWithIdentifier("MainNav", sender: self)
     }
-
+    
+    func showLoginController(){
+        performSegueWithIdentifier("LoginNav", sender: self)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
